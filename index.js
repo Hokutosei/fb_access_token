e47e018612d1a0fab058e800cf06391f89a1f75c
hokutosei <jeanepaul@gmail.com>
2014-03-12 09:48:31 +0000

    update

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ var request = require('./request.js')
 
 function main() {
   var last_token = '';
-    async.series([
-      function(callback) {
+  async.series({
+    findOne: function(callback) {
+        var start_find = new Date()
         model.findOne(function(err, resp) {
           if(err) throw err;
           //log(resp)
           last_token = resp.access_token
-          callback(null)
+          callback(null, (new Date() - start_find) + ' ms')
         })
       },
-      function(callback) {
+    requestToken: function(callback) {
         request.extendAccessToken(conf.postData(last_token), function(err, facebookRes) {
           var access_token = new model.AccessToken(facebookRes);
+          var start_save = new Date()
           access_token.save(function(err, resp) {
             //log(facebookRes)
             log(resp)
-            callback(null)
+            callback(null, (new Date() - start_save) + ' ms')
           })
         })      
       }
-    ], function(err, result) {
+  }, function(err, result) {
+      log(result)
       log('----------------------------------------------- ' + counter)
       counter++;
       setTimeout(main, 30000)    
