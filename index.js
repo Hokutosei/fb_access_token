15d65edf21790d559ead056dbcdbba5376eb6d94
hokutosei <jeanepaul@gmail.com>
2014-03-12 09:42:47 +0000

    update

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ var request = require('./request.js')
     , conf = require('./conf')
     , counter = 0
     , model = require('./model')
+    , async = require('async')
 
 function main() {
-  request.extendAccessToken(conf.postData, function(err, facebookRes) {
-    var access_token = new model.AccessToken(facebookRes);
-    access_token.save(function(err, resp) {
-      log(facebookRes)
+  var last_token = '';
+    async.series([
+      function(callback) {
+        model.findOne(function(err, resp) {
+          if(err) throw err;
+          //log(resp)
+          last_token = resp.access_token
+          callback(null)
+        })
+      },
+      function(callback) {
+        request.extendAccessToken(conf.postData(last_token), function(err, facebookRes) {
+          var access_token = new model.AccessToken(facebookRes);
+          access_token.save(function(err, resp) {
+            //log(facebookRes)
+            log(resp)
+            callback(null)
+          })
+        })      
+      }
+    ], function(err, result) {
       log('----------------------------------------------- ' + counter)
       counter++;
       setTimeout(main, 30000)    
     })
-    
-  })
 }
 
 main()
--- model.js
+++ model.js
@@ -8,4 +8,16 @@ var AccessToken = mongoose.model('AccessToken', {
                                                   , created_at: { type: Date, default: Date.now } 
                                                 })
 
-module.exports.AccessToken = AccessToken;
+//module.exports.AccessToken = AccessToken;
+
+module.exports = {
+  AccessToken: AccessToken,
+  
+  findOne: function(callback) {
+    console.log('called')
+    AccessToken.find({}, {}, {sort:{_id:-1}, limit:1}, function(err, resp) {
+      callback(null, resp[0])
+    })
+  }
+
+}
