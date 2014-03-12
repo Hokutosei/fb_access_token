1c6e4483bbbe134d2dc55ed5c72d7f83c50de337
hokutosei <jeanepaul@gmail.com>
2014-03-12 07:22:12 +0000

    update

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var request = require('./request.js')
     , log = function(str) { console.log(str) }
     , conf = require('./conf')
+    , counter = 0;
 
+function main() {
+  request.extendAccessToken(conf.postData, function(err, facebookRes) {
+    log(facebookRes)
+    log('----------------------------------------------- ' + counter)
+    counter++;
+    setTimeout(main, 5000)
+  })
+}
 
-/*
-request.postRequest({a: 'test'}, 'http://google.com', function(err, body, statuscode) {
-  if(err) throw err;
-  log(statuscode)
-
-})
-*/
-
-request.extendAccessToken(conf.postData, function(err, facebookRes) {
-  log(facebookRes)
-})
+main()
